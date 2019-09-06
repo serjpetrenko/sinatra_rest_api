db8ff0380ee003658d01e8080fd0f126bb482f91
serjpetrenko <b.g.lupus@gmail.com>
2019-09-06 21:33:25 +0300

    Exctract time for expiring token to method

--- helpers/json_web_token_helper.rb
+++ helpers/json_web_token_helper.rb
@@ -5,13 +5,17 @@ require 'time'
 module API
   module Helpers
     module JsonWebToken
-      def self.encode(payload, exp = Time.now + (24 * 60 * 60))
+      def self.encode(payload, exp = one_day)
         payload[:exp] = exp.to_i
         JWT.encode(payload, ENV['SECRET_KEY'])
       end
 
       def self.decode(token)
         JWT.decode(token, ENV['SECRET_KEY']).first
+      end
+
+      def self.one_day
+        Time.now + (24 * 60 * 60)
       end
     end
   end
